1042629134b6b014d36b3fd437e4e1294abb59b1
MatyasHroch <matyas.hroch01@upol.cz>
2025-11-09 22:13:01 +0100

    right redirect uri

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const clientId = "5715685e17054fdb818a86795f8bec1e";
-const redirectUri = "https://html-starter-theta-sand-69.vercel.app/"; // Your redirect uri
+const redirectUri = "intersongstionapp.vercel.app"; // Your redirect uri
 
 const codeVerifier = "some_random_string"; // TODO - veifier will be generated and stored per request
 const spotifyScopes =
