764d11fa203861597afbd84c678e1cf2e0da99b7
MatyasHroch <matyas.hroch01@upol.cz>
2025-11-09 22:11:59 +0100

    transformed into vue js app

---
+++ App.js
@@ -0,0 +1,82 @@
+const clientId = "5715685e17054fdb818a86795f8bec1e";
+const redirectUri = "https://html-starter-theta-sand-69.vercel.app/"; // Your redirect uri
+
+const codeVerifier = "some_random_string"; // TODO - veifier will be generated and stored per request
+const spotifyScopes =
+  "user-read-private user-read-email user-read-private user-read-email user-library-read playlist-read-private";
+
+const authEndpoint = "https://accounts.spotify.com/authorize";
+const spotifyTokenEndpoint = "https://accounts.spotify.com/api/token";
+
+async function exchangeToken(code) {
+  console.log("Exchanging code for token:", code);
+  // Here you would typically make a POST request to Spotify's token endpoint
+  // to exchange the authorization code for an access token.
+  const result = await fetch(spotifyTokenEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+    },
+    body: new URLSearchParams({
+      clientId,
+      grant_type: "authorization_code",
+      code,
+      redirectUri,
+      codeVerifier,
+    }),
+  });
+  return result.json();
+}
+
+export default {
+  template: `
+    <div>
+      <h1>{{ message }}</h1>
+      <button @click="count++">Clicked {{ count }} times</button>
+      <button @click="getNewIdentifier">Clicked {{ identifier }} times</button>
+      <button @click="logUser">Log new user in</button>
+    </div>
+  `,
+  data() {
+    return { message: "Hello Vue!", count: 0, users: [] };
+  },
+  methods: {
+    logUser() {
+      console.log("Redirects to spotify sign in page");
+      const authUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+        redirectUri
+      )}&scope=${encodeURIComponent(spotifyScopes)}&response_type=code`;
+      console.log(authUrl);
+      window.location.href = authUrl;
+    },
+    async handleSpotifyCallback() {
+      console.log("inspects URL for spotify code");
+      const args = new URLSearchParams(window.location.search);
+      const code = args.get("code");
+      if (code) {
+        console.log("Authorization code:", code);
+        // Here you would typically exchange the authorization code for an access token
+        const tokenData = await exchangeToken(code);
+        console.log("Token Data:", tokenData);
+      } else {
+        console.log("No authorization code found in the URL");
+      }
+    },
+    getNewIdentifier() {
+      this.newIdentifier = "user_" + this.users.length + 1;
+    },
+    loadUsers() {
+      console.log("Gets users and information from localStorage");
+    },
+    addUser() {
+      console.log("Adds a new user to localStorage and to the user list");
+    },
+    getLikedSongs(userIdentifier) {
+      console.log("gets liked songs from spotify API");
+    },
+  },
+  mounted() {
+    this.handleSpotifyCallback();
+    // this.loadUsers();
+  },
+};
--- script.js
+++ script.js
@@ -27,4 +27,6 @@ function handleCallback() {
   }
 }
 
+
+
 handleCallback();
